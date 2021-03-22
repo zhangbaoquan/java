511f646d35ae61b32e27665e1100086c75f06523
coffer <zbq19930108@126.com>
2021-03-22 21:25:04 +0800

    code message : 完善构建二叉树算法注释

--- src/tree/ReBuildTree.java
+++ src/tree/ReBuildTree.java
@@ -1,6 +1,7 @@
 package tree;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * 输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
@@ -9,16 +10,15 @@ import java.util.HashMap;
 
  例如，给出
 
- 前序遍历 preorder = [3,9,20,15,7]
+ 前序遍历 preorder =[3,9,20,15,7]
  中序遍历 inorder = [9,3,15,20,7]
  返回如下的二叉树：
 
- 3
- / \
- 9  20
- /  \
- 15   7
-  
+    3
+ /    \
+ 9    20
+    /    \
+   15     7
 
  限制：
 
@@ -32,31 +32,32 @@ public class ReBuildTree {
 
     //利用原理,先序遍历的第一个节点就是根。在中序遍历中通过根 区分哪些是左子树的，哪些是右子树的
     //左右子树，递归
-    HashMap<Integer, Integer> map = new HashMap<>();//标记中序遍历
-    int[] preorder;//保留的先序遍历
+    static HashMap<Integer, Integer> map = new HashMap<>();//标记中序遍历（主要是记录中序数组里的值的索引）
+    static int[] mPreorder;//保留的先序遍历
 
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        this.preorder = preorder;
-        for (int i = 0; i < preorder.length; i++) {
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+        mPreorder = preorder;
+        for (int i = 0; i < mPreorder.length; i++) {
+            // 这里的key 是中序数组的是值，value是值对应的索引，后面我们会用前序数组的值取找中序数组的索引
             map.put(inorder[i], i);
         }
-        return recursive(0,0,inorder.length-1);
+        return recursive(0,0,mPreorder.length-1);
     }
 
     /**
-     * @param pre_root_idx  先序遍历的索引
-     * @param in_left_idx  中序遍历的索引
-     * @param in_right_idx 中序遍历的索引
+     * @param pre_root_idx 根节点索引
+     * @param in_left_idx  左节点索引
+     * @param in_right_idx 右节点索引
      */
-    public TreeNode recursive(int pre_root_idx, int in_left_idx, int in_right_idx) {
+    public static TreeNode recursive(int pre_root_idx, int in_left_idx, int in_right_idx) {
         //相等就是自己
         if (in_left_idx > in_right_idx) {
             return null;
         }
         //root_idx是在先序里面的
-        TreeNode root = new TreeNode(preorder[pre_root_idx]);
+        TreeNode root = new TreeNode(mPreorder[pre_root_idx]);
         // 有了先序的,再根据先序的，在中序中获 当前根的索引
-        int idx = map.get(preorder[pre_root_idx]);
+        int idx = map.get(mPreorder[pre_root_idx]);
 
         //左子树的根节点就是 左子树的(前序遍历）第一个，就是+1,左边边界就是left，右边边界是中间区分的idx-1
         root.leftChild = recursive(pre_root_idx + 1, in_left_idx, idx - 1);
@@ -68,4 +69,9 @@ public class ReBuildTree {
         root.rightChild = recursive(pre_root_idx + (idx-1 - in_left_idx +1)  + 1, idx + 1, in_right_idx);
         return root;
     }
+
+    public static void main(String[] args) {
+        buildTree(new int[]{3,9,20,15,7},new int[]{9,3,15,20,7});
+    }
+
 }
